9c09f4ecb90268b45283bfca7d17b08ad51297f2
Royhan Anwar <anwarroyhan@gmail.com>
2018-04-24 22:04:50 +0700

    finish models and add flask migrate and run it

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Employee(UserMixin, db.Model):
     first_name = db.Column(db.String(60), index=True)
     last_name = db.Column(db.String(60), index=True)
     password_hash = db.Column(db.String(128))
-    departmen_id = db.Column(db.Integer, db.ForeignKey('departmens.id'))
+    departmen_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     is_admin = db.Column(db.Boolean, default=False)
 
@@ -42,4 +42,42 @@ class Employee(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<Employee: {}'.format(self.username)
+        return '<Employee: {}>'.format(self.username)
+
+
+# setup user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return Employee.query.get(user_id)
+
+
+class Department(UserMixin, db.Model):
+    '''
+    Department Tabel
+    '''
+
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='department', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Department: {}>'.format(self.name)
+
+
+class Role(UserMixin, db.Model):
+    '''
+    Role Tabel
+    '''
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
